bdd38646fa4075fd83aa9c8be9868c292c57e9c6
rian-sm <riansm100@gmail.com>
2025-07-25 03:52:38 -0300

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
 
+const isProd = process.env.DEPLOY_ENV || false
+
+console.log('Vite configuration loaded. Production mode:', process.env.DEPLOY_ENV);
 // https://vite.dev/config/
 export default defineConfig({
+  base: isProd ? '/ebook-loteria-lp/' : './',
   plugins: [
     react(),
     tailwindcss(),
